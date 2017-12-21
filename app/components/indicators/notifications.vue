08d1d17572e7c7da0f27e69f13d7dbad75b79b0c
Quentin de Longraye <quentin@dldl.fr>
2017-12-21 11:07:17 +0100

    notifications interaction in indicators

--- app/components/indicators/notifications.vue
+++ app/components/indicators/notifications.vue
@@ -1,29 +1,54 @@
 <template>
   <div>
     <template v-if="!supported">
+      <icon icon="bell-slash" ref="unsupported" title="Notifications not supported" class="text-warning"></icon>
+    </template>
+    <template v-else-if="denied">
       <icon icon="bell-slash"></icon>
     </template>
+    <template v-else-if="granted">
+      <icon icon="bell"></icon>
+    </template>
     <template v-else>
-      <a href="#" @click.prevent="enableNotifications">
-        <icon icon="bell"></icon>
+      <a href="#" @click.prevent="enableNotifications" ref="enable" title="Click here to enable notifications" class="text-info">
+        <icon icon="bell-slash"></icon>
       </a>
     </template>
   </div>
 </template>
 
 <script>
+  import tippy from 'tippy.js'
+
   export default {
     name: 'notifications',
     data () {
       return {
-        supported: 'Notification' in window
+        supported: 'Notification' in window,
+        granted: Notification.permission === 'granted',
+        denied: Notification.permission === 'denied'
       }
     },
     methods: {
       enableNotifications () {
-        if (Notification.permission !== 'denied' && Notification.permission !== 'granted') {
-          Notification.requestPermission()
-        }
+        Notification.requestPermission().then((result) => {
+          if (result === 'denied') {
+            this.denied = true
+          }
+          if (result === 'granted') {
+            this.granted = true
+          }
+        })
+      }
+    },
+    mounted () {
+      tippy('[title]', {
+        animation: 'fade',
+        arrow: true
+      })
+
+      if (Notification.permission !== 'denied' && Notification.permission !== 'granted') {
+        this.$refs.enable._tippy.show()
       }
     }
   }
