86ce060d032e55feff7c53cb0dfe5f8b6128571f
Coac <levictor3000@hotmail.fr>
2017-12-25 11:39:51 +0100

    Detect correctly node env for fetch

--- explorerLibName/explorers/AbstractExplorer.js
+++ explorerLibName/explorers/AbstractExplorer.js
@@ -193,7 +193,9 @@ class AbstractExplorer {
   }
 
   static async _fetchJson (url) {
-    // TODO : require('node-fetch')
+    if (typeof process === 'object') {
+      return require('node-fetch')(url).then((response) => response.json())
+    }
     return fetch(url).then((response) => response.json())
   }
 }
