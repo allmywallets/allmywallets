e1d2770eeaf96b6cc9292f73d007521536f93e77
Quentin de Longraye <quentin@dldl.fr>
2017-12-25 09:01:34 +0100

    wip qr code export

--- app/components/app-settings.vue
+++ app/components/app-settings.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h2>Application settings</h2>
+    <h3>Customization</h3>
+    <p>TBD</p>
+    <h3>Export configuration</h3>
+    <p>
+      You can export your configuration to another device by scanning the above QRCode or accessing the url from the
+      targeted device. This will replace its configuration with the current configuration of All My Wallets; it will
+      export your wallets and customization settings.
+    </p>
+    <canvas ref="qrcode"></canvas>
     <config-expert></config-expert>
     <h2>Danger zone</h2>
     <a href="#" @click.prevent="forceUpdate">Force app update</a>
@@ -9,6 +18,7 @@
 
 <script>
   import ConfigExpert from './config-expert.vue'
+  import QRCode from 'qrcode'
 
   export default {
     name: 'app-settings',
@@ -17,13 +27,16 @@
     },
     methods: {
       forceUpdate () {
-        navigator.serviceWorker.getRegistrations().then((registrations) => {
+        this.$serviceWorker.getRegistrations().then((registrations) => {
           for (const registration of registrations) {
             registration.unregister()
           }
         })
         location.reload()
       }
+    },
+    mounted () {
+      QRCode.toCanvas(this.$refs.qrcode, 'hello world!', () => {})
     }
   }
 </script>
