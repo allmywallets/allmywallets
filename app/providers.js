42dd818f5e213362620b6f25afb950c88cf89399
Coac <levictor3000@hotmail.fr>
2017-12-26 14:42:35 +0100

    Removed the providers folder, use only providers.js

--- app/providers.js
+++ app/providers.js
@@ -1,10 +1,12 @@
-import GenericProvider from './providers/GenericProvider'
+import Balance from './model/Balance'
+import Transaction from './model/Transaction'
+import ExplorerLib from '../explorerLibName'
 
 const providers = {
-  'bitcoin.blockexplorer': GenericProvider('BitcoinBlockExplorer'),
-  'bitcoin.mockexplorer': GenericProvider('MockExplorer'),
-  'ethereum.etherscan': GenericProvider('EthereumEtherscan'),
-  'iota.native': GenericProvider('IOTA')
+  'bitcoin.blockexplorer': 'BitcoinBlockExplorer',
+  'bitcoin.mockexplorer': 'MockExplorer',
+  'ethereum.etherscan': 'EthereumEtherscan',
+  'iota.native': 'IOTA'
 }
 
 export default class Proxy {
@@ -15,10 +17,62 @@ export default class Proxy {
       throw new Error(`Unsupported network provider: ${networkProvider}`)
     }
 
-    return new providers[networkProvider](parameters)
+    const Provider = getGenericProviderClass(providers[networkProvider])
+    return new Provider(parameters)
   }
 
   static getProvidersList () {
     return providers
   }
+}
+
+function getGenericProviderClass (explorerName) {
+  const Explorer = ExplorerLib.explorer(explorerName)
+  class GenericProvider {
+    constructor (parameters) {
+      this.parameters = parameters
+      this.explorer = new Explorer(this.parameters.explorerSpecific)
+    }
+
+    async getWalletData (currencies = []) {
+      this._selectCurrenciesToUpdate(this.explorer, currencies)
+      const wallets = await this.explorer
+        .addresses(this.parameters.addresses)
+        .fetch(['balances', 'transactions'])
+        .exec()
+
+      const balances = []
+      wallets.forEach(wallet => {
+        let i = 0
+        this.explorer.getSelectedCurrencies().forEach(selectedCurrency => {
+          const amount = wallet.balances[i]
+          const transactions = wallet.transactions[i].map(tx => new Transaction(tx.type, tx.from, tx.to, tx.amount))
+          const balance = new Balance(selectedCurrency.name, selectedCurrency.ticker, amount, new Date(), transactions)
+          balances.push(balance)
+          ++i
+        })
+      })
+      return balances
+    }
+
+    _selectCurrenciesToUpdate (explorer, currencies) {
+      if (currencies.length === 0) { // Refresh all
+        if (this.parameters.currencies) {
+          explorer.currencies(this.parameters.currencies)
+        }
+      } else { // Refresh only specified
+        explorer.currencies(currencies)
+      }
+    }
+
+    static getSupportedParameters () {
+      const explorerParams = Explorer.getExplorerParams().map(param => {
+        param.model = `explorerSpecific.${param.model}`
+        return param
+      })
+      return explorerParams.concat(Explorer.getAddressParam())
+    }
+}
+
+  return GenericProvider
 }
