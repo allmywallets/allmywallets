a874d58685379d8bcc2b76b34645bafc8166dbab
Quentin de Longraye <quentin@dldl.fr>
2018-01-18 16:24:20 +0100

    add holdings movements

--- app/components/HoldingsValue.vue
+++ app/components/HoldingsValue.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="holdings-value">
-    <span class="holdings-amount">
-      ${{ currentHoldings|toFixed(2) }}
-    </span><br />
-    <span class="holdings-secondary-amount">
-      <span class="ticker">BTC</span>0
-    </span>
+    <span class="holdings-amount">${{ currentHoldings|toFixed(2) }}</span>
+    <span class="holdings-secondary-amount"><span class="ticker">BTC</span>0</span><br />
+    <span :class="{ 'movement': true, 'decrease': lastMovement < 0 }">{{ lastMovement|toFixed(1) }}%</span>
   </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import { sumHoldingsHistories } from '../manager/holdings-manager'
 
   export default {
     name: 'holdings-value',
     computed: {
       ...mapGetters([
-        'currentHoldings'
-      ])
+        'currentHoldings',
+        'holdingsHistory'
+      ]),
+      lastMovement () {
+        const holdingsSum = sumHoldingsHistories(this.holdingsHistory)
+
+        return holdingsSum[holdingsSum.length - 1] / holdingsSum[holdingsSum.length - 2] * 100 - 100
+      }
     }
   }
 </script>
@@ -30,7 +34,7 @@
     text-align: center;
     padding-top: 100px;
     color: $color-primary;
-    line-height: 2rem;
+    line-height: 1rem;
     position: relative;
     z-index: 1;
     pointer-events: none;
@@ -39,8 +43,28 @@
       font-size: 5rem;
     }
 
+    .movement {
+      font-size: 1.2rem;
+      color: $color-success;
+      font-family: $font-default;
+
+      &:before {
+        content: '▲';
+        vertical-align: 1px;
+      }
+
+      &.decrease {
+        color: $color-danger;
+
+        &:before {
+          content: '▼';
+        }
+      }
+    }
+
     .holdings-secondary-amount {
       font-size: 2rem;
+      line-height: 2.4rem;
 
       svg {
         font-size: 1rem;
