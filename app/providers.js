536ea22f27c635fa1d0d14a96b369f16336cc3a3
Coac <levictor3000@hotmail.fr>
2018-01-28 17:53:09 +0100

    Fix warnings attributes name

--- app/providers.js
+++ app/providers.js
@@ -26,7 +26,7 @@ export default class Proxy {
   static getProvidersList () {
     return Providers.list().map(provider => {
       const info = Providers.providers[provider].info
-      info.warnings = {hasCORS: info.hasCORS, apiKeyPermission: info.apiKeyPermission}
+      info.warnings = {cors: info.hasCORS, apiKey: info.apiKeyPermission}
       delete info.hasCORS
       delete info.apiKeyPermission
       return info
