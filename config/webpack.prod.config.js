e26bd780d692fc0b4832f2e7cc52914799f8908c
Quentin de Longraye <quentin@dldl.fr>
2018-11-13 23:37:16 +0100

    fix build for functional tests

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,12 +1,10 @@
 const webpack = require('webpack')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
 
 module.exports = env => merge(base(env), {
   plugins: [
     new webpack.optimize.AggressiveMergingPlugin(),
-    new UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"',
