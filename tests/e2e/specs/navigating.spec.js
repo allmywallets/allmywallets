ce8653e326c6607bcaaeb90f6ece825a6bebb85e
Quentin de Longraye <quentin@dldl.fr>
2021-03-14 18:33:30 +0100

    add cypress eslint custom config

--- tests/e2e/specs/navigating.spec.js
+++ tests/e2e/specs/navigating.spec.js
@@ -1,9 +1,6 @@
-context('Navigating', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
+describe('Navigating', () => {
   it('show welcome', () => {
+    cy.visit('/')
     cy.get('h2').contains('Welcome to AllMyWallets!')
   })
 })
